25a4955794844891a3a80d86ea30565e37a3e155
Christian Opitz <christian.opitz@netresearch.de>
2019-02-14 16:04:48 +0100

    Made fallback to ipty.de/feiertag/api actually work

--- src/store/modules/holidays.js
+++ src/store/modules/holidays.js
@@ -19,33 +19,26 @@ export default {
         throw new Error('State needs to be configured')
       }
       if (!state.holidays[year]) {
+        const holidays = new Holidays()
         commit('holidays', {
           year,
           holidays: axios.get(`https://feiertage-api.de/api/?jahr=${year}&nur_land=${st.toUpperCase()}`).then(
-            res => {
-              const holidays = new Holidays()
-              Object.keys(res.data).forEach(title => {
-                const holiday = res.data[title]
-                if (!holiday.hinweis) {
-                  holidays[holiday.datum] = title
-                }
-              })
-              commit('holidays', {year, holidays})
-              return holidays
-            },
-            ftaErr => axios.get(`https://ipty.de/feiertag/api.php?do=getFeiertage&loc=${st.toUpperCase()}&jahr=${year}&outformat=Y-m-d`).then(
-              res => {
-                const holidays = new Holidays()
-                res.data.forEach(holiday => {
-                  holidays[holiday.date] = holiday.title
-                })
-              },
-              iptyErr => {
-                console.error('Could not load holidays', ftaErr, iptyErr)
-                commit('holidays', {year, holidays: new Holidays()})
+            res => Object.keys(res.data).forEach(title => {
+              const holiday = res.data[title]
+              if (!holiday.hinweis) {
+                holidays[holiday.datum] = title
               }
+            }),
+            ftaErr => axios.get(`https://ipty.de/feiertag/api.php?do=getFeiertage&loc=${st.toUpperCase()}&jahr=${year}&outformat=Y-m-d`).then(
+              res => res.data.forEach(holiday => {
+                holidays[holiday.date] = holiday.title
+              }),
+              iptyErr => console.error('Could not load holidays', ftaErr, iptyErr)
             )
-          )
+          ).then(() => {
+            commit('holidays', {year, holidays})
+            return holidays
+          })
         })
       }
       return state.holidays[year]
